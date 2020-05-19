fe4524cbbabd370b4826107646219115244a3c64
shenyunjie <shenyunjie@aidigger.com>
2020-05-19 16:35:15 +0800

    udpate

--- packages/slate/src/transforms/index.ts
+++ packages/slate/src/transforms/index.ts
@@ -1,5 +1,5 @@
-export * from './html';
 export * from './elementRender';
 export * from './leafElement';
 export * from '../components/editable';
 export * from '../components/provider';
+export * from '../components/toolbar';
