fd7450ad1eba66889e6e0491e07f354c46091799
shenyunjie <shenyunjie@aidigger.com>
2020-05-19 17:57:40 +0800

    update

---
+++ build/precache-manifest.e9d406ea755f7dca8b4d836cf9e78cf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aac58dcc39b637eabd16d87d43e01cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47c3a4dfde9a9aad6fed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9677541af598666b456c",
+    "url": "/static/js/2.bd04d9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bd04d9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c3a4dfde9a9aad6fed",
+    "url": "/static/js/main.ba420c8c.chunk.js"
+  },
+  {
+    "revision": "a03d9aacb730d3a1db8c",
+    "url": "/static/js/runtime-main.00185f09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
