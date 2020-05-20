1239c13315b27628e227501d700c87314a6c6820
shenyunjie <shenyunjie@aidigger.com>
2020-05-20 16:18:29 +0800

    update

--- scripts/create.js
+++ scripts/create.js
@@ -165,7 +165,7 @@ function createRollup(dir) {
   const template = `import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 export default {
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
