df8c0114c26c22b6bed258552e0e9b8623316b5f
shenyunjie <shenyunjie@aidigger.com>
2020-05-20 16:25:27 +0800

    update

--- scripts/create.js
+++ scripts/create.js
@@ -102,54 +102,54 @@ function createDir(dir, name) {
 function createIndexFile(src, project) {
   const name = project[0].toUpperCase() + project.substring(1);
   fs.writeFileSync(path.resolve(src, 'index.tsx'), `import React from 'react';
-  import { ReactEditor } from 'slate-react';
-  import { Subscription } from '@reactivex/rxjs';
-  import { SlateFunction, TSlateFunction, SlateContainer, TLeafRenderProps, TElementRenderProps } from '@slatable/slate';
-  
-  export class ${name}Function extends SlateFunction implements TSlateFunction {
-    static readonly namespace = '${name}';
-    private readonly event$: Subscription;
-    constructor(container: SlateContainer) {
-      super(container, 'leaf');
-      this.event$ = this.container.on('editor:' + ${name}Function.namespace).subscribe(() => {
-        this.container.focus();
-        this.setLeaf(${name}Function.namespace);
-      });
-    }
-  
-    // public componentRenderNodes(props: TLeafRenderProps | TElementRenderProps, style: { [key: string]: any }, data: any) {
-      return <div></div>
-    }
-  
-    // public componentRenderStyle<T = any>(data?: T): {
-      [key: string]: string | number | boolean,
-    } {
-      return {}
-    }
-  
-    // public componentWithWrapper(editor: ReactEditor): ReactEditor {
-      return editor;
-    }
-  
-    public componentTerminate(): void {
-      this.event$.unsubscribe();
-    }
-  
-    // public useRenderHook<R = any>(
-      container: SlateContainer, 
-      props: TLeafRenderProps | TElementRenderProps
-    ): R {
-      return;
-    }
-  
-    // public useRangeMardHook<T = any>(value: T): boolean {
-      return false;
-    }
-  
-    // public componentDeserialize<T extends HTMLElement>(el: T): { [key: string]: any } {
-      return {};
-    }
-  }`, 'utf8');
+import { ReactEditor } from 'slate-react';
+import { Subscription } from '@reactivex/rxjs';
+import { SlateFunction, TSlateFunction, SlateContainer, TLeafRenderProps, TElementRenderProps } from '@slatable/slate';
+
+export class ${name}Function extends SlateFunction implements TSlateFunction {
+  static readonly namespace = '${name}';
+  private readonly event$: Subscription;
+  constructor(container: SlateContainer) {
+    super(container, 'leaf');
+    this.event$ = this.container.on('editor:' + ${name}Function.namespace).subscribe(() => {
+      this.container.focus();
+      this.setLeaf(${name}Function.namespace);
+    });
+  }
+
+  // public componentRenderNodes(props: TLeafRenderProps | TElementRenderProps, style: { [key: string]: any }, data: any) {
+  //   return <div></div>
+  // }
+
+  // public componentRenderStyle<T = any>(data?: T): {
+  //   [key: string]: string | number | boolean,
+  // } {
+  //   return {}
+  // }
+
+  // public componentWithWrapper(editor: ReactEditor): ReactEditor {
+  //   return editor;
+  // }
+
+  public componentTerminate(): void {
+    this.event$.unsubscribe();
+  }
+
+  // public useRenderHook<R = any>(
+  //   container: SlateContainer, 
+  //   props: TLeafRenderProps | TElementRenderProps
+  // ): R {
+  //   return;
+  // }
+
+  // public useRangeMardHook<T = any>(value: T): boolean {
+  //   return false;
+  // }
+
+  // public componentDeserialize<T extends HTMLElement>(el: T): { [key: string]: any } {
+  //   return {};
+  // }
+}`, 'utf8');
 }
 
 function createTestFile(test, project) {
